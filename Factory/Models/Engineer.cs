c4769fe177d524120206cffb4e2706f7b01e2281
Kari Vigna <karivigna24@gmail.com>
2023-10-20 09:46:39 -0700

    views and css updates

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -9,6 +9,8 @@ namespace Factory.Models
         public int EngineerId { get; set; }
         [Required(ErrorMessage = "The engineer's name cannot be empty!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Enter the hire date.")]
+        [DataType(DataType.Date)]
         public DateTime HireDate { get; set; }
         public List<EngineerMachine> JoinEntities { get; set; }
     }
